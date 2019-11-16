532b6c71cc1d7c92350efd36fb6085278bf73b70
zavin <zavinhussein@gmail.com>
2019-11-16 19:15:22 +0200

    Updates

---
+++ precache-manifest.8610821b81ba0d86a8669dbfe85229a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dcf75cf4c19de158207c27080a2cc4",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "351013531af30130adc5",
+    "url": "/portfolio-website/static/css/main.4ea07990.chunk.css"
+  },
+  {
+    "revision": "8e2a8b72c1168a7d4f73",
+    "url": "/portfolio-website/static/js/2.744eda66.chunk.js"
+  },
+  {
+    "revision": "351013531af30130adc5",
+    "url": "/portfolio-website/static/js/main.437978dc.chunk.js"
+  },
+  {
+    "revision": "a323bacbc728a96ddec8",
+    "url": "/portfolio-website/static/js/runtime-main.433b3a51.js"
+  },
+  {
+    "revision": "33635f6d6353688820073fbc0426e9a9",
+    "url": "/portfolio-website/static/media/Autumn_fireball.33635f6d.jpg"
+  },
+  {
+    "revision": "700a13e875ad825362999479701e2a3f",
+    "url": "/portfolio-website/static/media/Burger_builder.700a13e8.png"
+  },
+  {
+    "revision": "68965b1401bb4b5daae3213d4bd94948",
+    "url": "/portfolio-website/static/media/To-do-list.68965b14.jpg"
+  },
+  {
+    "revision": "927786b70c7c701c24ecc7e34f97d025",
+    "url": "/portfolio-website/static/media/portrait.927786b7.jpg"
+  }
+]);
